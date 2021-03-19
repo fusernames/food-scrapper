7738d32a749c8fe117998b2e0033cf52f6d98f1d
fusernames <acaroff10@gmail.com>
2021-03-19 02:11:39 +0100

    Added tripadvisor

--- scrapper.js
+++ scrapper.js
@@ -4,51 +4,80 @@ const cheerio = require('cheerio')
 const config = {
   headers: {
     'Accept-Charset': 'utf-8',
-    // 'Accept-Language': 'en-US;q=0.5,en;q=0.3',
+    'Accept-Language': 'en-US,en;q=0.8,en-GB;q=0.8,*;q=0.7',
     'User-Agent': 'Mozilla/5.0',
   },
 }
 
 const main = async () => {
-  const query = 'como kitchen'
+  let args = process.argv.slice(2)
+  const query = args[0] || 'como kitchen'
+  console.log('args =>', args)
   console.log(`0. recherche pour ===> "${query}"`)
 
-  const [deliveroo, ubereats] = await Promise.all([scrapDeliveroo(query), scrapUbereats(query)])
-  console.log(`1. deliveroo url ===> ${deliveroo.url}`)
-  console.log(`1. deliveroo title ===> ${deliveroo.title}`)
-  console.log(`1. deliveroo rating ===> ${deliveroo.rating}`)
-  console.log(`1. deliveroo count ===> ${deliveroo.count}`)
-  console.log(`2. ubereats url ===> ${ubereats.url}`)
-  console.log(`2. ubereats title ===> ${ubereats.title}`)
-  console.log(`2. ubereats rating ===> ${ubereats.rating}`)
-  console.log(`2. ubereats count ===> ${ubereats.count}`)
+  const [deliveroo, ubereats, tripadvisor] = await Promise.all([
+    scrapDeliveroo(query),
+    scrapUbereats(query),
+    scrapTripadvisor(query),
+  ])
+
+  console.log(`1. deliveroo ${JSON.stringify(deliveroo, null, 4)}`)
+  console.log(`2. ubereats ${JSON.stringify(ubereats, null, 4)}`)
+  console.log(`3. tripadvisor ${JSON.stringify(tripadvisor, null, 4)}`)
   return 0
 }
 
-const scrapUbereats = async (query) => {
-  const urls = await search(query + ' uber eats')
-  const url = urls[0]
-  const res = await axios.get(url, config)
-  const $ = cheerio.load(res.data)
-  const title = $('h1').text()
-  let structuredData = $('script[type="application/ld+json"]')[0].children[0].data
-  structuredData = JSON.parse(structuredData)
-  const rating = structuredData.aggregateRating.ratingValue
-  const count = structuredData.aggregateRating.reviewCount
+const scrapTripadvisor = async (query) => {
+  try {
+    const urls = await search(query + ' tripadvisor')
+    const url = urls[0]
+    const res = await axios.get(url, config)
+    const $ = cheerio.load(res.data)
+    const ratingElem = $('a[href="#REVIEWS"]').last()
+    const rating = Number(ratingElem.prev().prev().text()) // works with US format only
+    const count = Number(ratingElem.text().split(' ')[0])
+    let structuredData = getStructuredData($)
+    const title = structuredData.name
+    const address = structuredData.address
+    return { title, rating, count, url, address }
+  } catch (e) {
+    return null
+  }
+}
 
-  return { url, title, rating, count }
+const scrapUbereats = async (query) => {
+  try {
+    const urls = await search(query + ' uber eats')
+    const url = urls[0]
+    const res = await axios.get(url, config)
+    const $ = cheerio.load(res.data)
+    // const title = $('h1').text()
+    let structuredData = getStructuredData($)
+    const rating = structuredData.aggregateRating.ratingValue
+    const count = structuredData.aggregateRating.reviewCount
+    const title = structuredData.name
+    const address = structuredData.address
+
+    return { title, rating, count, url, address }
+  } catch (e) {
+    return null
+  }
 }
 
 const scrapDeliveroo = async (query) => {
-  const urls = await search(query + ' deliveroo')
-  const url = urls[0]
-  const res = await axios.get(url, config)
-  const $ = cheerio.load(res.data)
-  const title = $('h1').text()
-  const rating = $('h1').next().text().split('(')[0]
-  const count = $('h1').next().text().split('(')[1].split('+')[0]
+  try {
+    const urls = await search(query + ' deliveroo')
+    const url = urls[0]
+    const res = await axios.get(url, config)
+    const $ = cheerio.load(res.data)
+    const title = $('h1').text()
+    const rating = $('h1').next().text().split('(')[0]
+    const count = $('h1').next().text().split('(')[1].split('+')[0]
 
-  return { rating, count, title, url }
+    return { title, rating, count, url }
+  } catch (e) {
+    return null
+  }
 }
 
 const search = async (query) => {
@@ -63,5 +92,7 @@ const search = async (query) => {
   })
   return urls
 }
+
+const getStructuredData = ($) => JSON.parse($('script[type="application/ld+json"]')[0].children[0].data)
 
 main()
